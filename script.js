c3dddad7e123f562c43f810c18ffee64fd9d22c0
Gulherme <guilhermemiller.dev@gmail.com>
2024-10-11 14:07:22 -0300

    Add files via upload
    Nesse projeto foi desenvolvido um site para consultar a previsão do tempo.

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+ const key = "847e13edd8fa34b0df67fb4df4eaef6b"
+
+
+ function colocarDadosNaTela(dados) {
+    console.log(dados)
+    document.querySelector(".cidade").innerHTML = "tempo em " + dados.name
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°c"
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".Umidade").innerHTML = "Umidade: " + dados.main.humidity + "%"
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+ } 
+
+
+async function buscarCidade(cidade) {
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then( resposta => resposta.json())
+
+    colocarDadosNaTela(dados)
+   
+}    
+
+function cliqueiNoBotao() {
+    const cidade = document.querySelector(".input-cidade").value
+
+    buscarCidade(cidade)
+} 
